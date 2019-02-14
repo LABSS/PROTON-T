d1d408771798ac673ac7132c5b158baf76301701
mariopaolucci <mario.paolucci@gmail.com>
2019-02-14 18:28:12 +0100

    Working extraction from new neukolln files
    The files aren't yet used in the model, tho

--- inputs/neukolln/extract_Neighborhoods_MGAR.R
+++ inputs/neukolln/extract_Neighborhoods_MGAR.R
@@ -4,7 +4,7 @@ library(readxl)
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 
 maxcol<-37+4*4
-#maxcol<-217
+maxcol<-217
 
 df <-
   file.path("raw", "Neukolln population statistics (citizenship - migrant background, gender, age, religion).xlsx") %>%
@@ -25,9 +25,8 @@ for(i in rev(2:maxcol)) {
   l<-4;r<-3
   df0[r,paste0("X__",i)] <- paste0(trimws(df0[r-1,paste0("X__",i)]),"___", 
                                    df0[r,trimws(paste0("X__",floor((i-2)/l)*l+2))],"___",
-                                   df0[r+1,trimws(paste0("X__",floor((i-2)/l)*l+2))])
+                                   df0[r+1,trimws(paste0("X__",i))])
 }
-
 # for(i in rev(2:maxcol)) {
 #   l<-36
 #   print(paste0("X__",i,"X__",floor((i-2)/l)*l+2))
@@ -35,13 +34,17 @@ for(i in rev(2:maxcol)) {
 
 df0[3,1]<-"area"
 names(df1)<-df0[3,]
-length(df0[3,])
-length(unique(df0[3,]))
 
 
-df1 <- df1 %>%
-  gather(class, var, -area)
-
-  
-  
-  
+df3 <-   df1 %>%
+  gather(class, var, -area) %>%
+  separate(class, into = c("a", "b", "age","religion"), sep = "___") %>%
+  mutate(
+    age = case_when(
+      age == "unter 1 Jahr" ~ "0 bis unter 1",
+      age == "80 und mehr" ~ "80 bis unter 120",
+      TRUE ~ age
+    )
+  ) %>%
+  separate(age, into = c("age_from", "age_to"), sep = " bis unter ") %>%
+  write_csv(file.path("data", "neukolln-by-citizenship-migrantbackground-gender-age-religion.csv"))  
