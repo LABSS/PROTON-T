1cfb941e5ba9c3ad7991c1f934137c50662dcf33
Mario <mario.paolucci@gmail.com>
2019-02-20 18:13:56 +0100

    created docker directory

---
+++ docker/runit-T.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# argument is release version
+
+# this is the simulation
+cp docker/exp-T.xml .
+/opt/netlogo/netlogo-headless.sh --model PROTON-T.nlogo --setup-file exp-T.xml --table table-output.csv
+
+# now we save both result and experiment file
+#https://unix.stackexchange.com/questions/340010/how-do-i-create-sequentially-numbered-file-names-in-bash
+
+today="$( date +"%Y%m%d" )"
+number=0
+
+while test -e "/extdisk/$1/$today$suffix"; do
+    (( ++number ))
+    suffix="$( printf -- '-%02d' "$number" )"
+done
+
+dname="$today$suffix"
+
+printf 'Will use "%s" as dirname\n' "$dname"
+mkdir -p "/extdisk/T/$1/$today$suffix/"
+
+cp table-output.csv "/extdisk/T/$1/$today$suffix/"
+cp exp-T.xml "/extdisk/$1/T/$today$suffix/"
+
