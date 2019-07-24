9eaf13e9fa0f34d67a1ce2f10a32dec610cbde6e
mariopaolucci <mario.paolucci@gmail.com>
2019-07-24 14:52:24 +0200

    New setup for experiments, that better respects the rp0.6 description

--- experiments-xml/makeSensitivityAndInterventions.py
+++ experiments-xml/makeSensitivityAndInterventions.py
@@ -21,22 +21,102 @@ def indent(elem, level=0):
 tree = ET.parse('base_rp0.6.xml')
 root = tree.getroot()
 
-#update all price
-for price in root.iter('enumeratedValueSet'):
-	new_price = "int(price.text) + 5"
-	price.text = str(new_price)
-	price.set('updated', 'yes')
+# first, sentitivity analysis.
 
-for child in root.iter('enumeratedValueSet'):
-  print(child.tag, child.attrib)
+al = tree.find('.//enumeratedValueSet[@variable="activity-radius"]')
+al.insert(1, ET.Element("value", value="5"))
 
-for vals in root.iter('enumeratedValueSet'):
-   name = vals.find('variable').text
-   print(name)
-   if name == 'total-citizens':
-   		print('found') 
-	
+al = tree.find('.//enumeratedValueSet[@variable="alpha"]')
+al.insert(1, ET.Element("value", value="0.0"))
+
+al = tree.find('.//enumeratedValueSet[@variable="recruit-hours-threshold"]')
+al.insert(1, ET.Element("value", value="50"))
+
+al = tree.find('.//enumeratedValueSet[@variable="population-employed-%"]')
+al.insert(1, ET.Element("value", value="5"))
+al.insert(1, ET.Element("value", value="20"))
+al.insert(1, ET.Element("value", value="40"))
+
+al = tree.find('.//enumeratedValueSet[@variable="work-socialization-probability"]')
+al.insert(1, ET.Element("value", value="0.3"))
+
+al = tree.find('.//enumeratedValueSet[@variable="male-ratio"]')
+al.insert(1, ET.Element("value", value="45"))
+al.insert(1, ET.Element("value", value="55"))
+
 #write to file
 tree = ET.ElementTree(indent(root))
-tree.write('bookstore2.xml', xml_declaration=True, encoding='utf-8')
+tree.write('sensitivity_rp0.6.xml', encoding='utf-8')
+
+# then we restart and prepare the high risk
+
+tree = ET.parse('base_rp0.6.xml')
+root = tree.getroot()
+
+al = tree.find('.//enumeratedValueSet[@variable="high-risk-employed"]')
+for x in al.getchildren():
+  al.remove(x)
+al.insert(1, ET.Element("value", value="50"))
+al.insert(1, ET.Element("value", value="100"))
+
+al = tree.find('.//enumeratedValueSet[@variable="population-employed-%"]')
+al.insert(1, ET.Element("value", value="5"))
+al.insert(1, ET.Element("value", value="20"))
+al.insert(1, ET.Element("value", value="40"))
+
+al = tree.find('.//enumeratedValueSet[@variable="male-ratio"]')
+al.insert(1, ET.Element("value", value="45"))
+al.insert(1, ET.Element("value", value="55"))
+
+#write to file
+tree = ET.ElementTree(indent(root))
+tree.write('high-risk_rp0.6.xml',  encoding='utf-8')
+
+# then we restart and prepare the CPOS
+
+tree = ET.parse('base_rp0.6.xml')
+root = tree.getroot()
+
+al = tree.find('.//enumeratedValueSet[@variable="cpo-%"]')
+for x in al.getchildren():
+  al.remove(x)
+al.insert(1, ET.Element("value", value="25"))
+al.insert(1, ET.Element("value", value="50"))
+
+al = tree.find('.//enumeratedValueSet[@variable="population-employed-%"]')
+al.insert(1, ET.Element("value", value="5"))
+al.insert(1, ET.Element("value", value="20"))
+al.insert(1, ET.Element("value", value="40"))
+
+al = tree.find('.//enumeratedValueSet[@variable="male-ratio"]')
+al.insert(1, ET.Element("value", value="45"))
+al.insert(1, ET.Element("value", value="55"))
+
+#write to file
+tree = ET.ElementTree(indent(root))
+tree.write('cpos_rp0.6.xml', encoding='utf-8')
+
+# finally, the community workers 
+
+tree = ET.parse('base_rp0.6.xml')
+root = tree.getroot()
+
+al = tree.find('.//enumeratedValueSet[@variable="number-workers-per-community-center"]')
+for x in al.getchildren():
+  al.remove(x)
+al.insert(1, ET.Element("value", value="3"))
+al.insert(1, ET.Element("value", value="5"))
+
+al = tree.find('.//enumeratedValueSet[@variable="population-employed-%"]')
+al.insert(1, ET.Element("value", value="5"))
+al.insert(1, ET.Element("value", value="20"))
+al.insert(1, ET.Element("value", value="40"))
+
+al = tree.find('.//enumeratedValueSet[@variable="male-ratio"]')
+al.insert(1, ET.Element("value", value="45"))
+al.insert(1, ET.Element("value", value="55"))
+
+#write to file
+tree = ET.ElementTree(indent(root))
+tree.write('communityworkers_rp0.6.xml', encoding='utf-8')
 
