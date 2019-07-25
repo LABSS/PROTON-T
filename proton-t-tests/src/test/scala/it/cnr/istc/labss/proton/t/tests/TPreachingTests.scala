8c0bc007304eed4d0d1f366e7bdf4e09d82652c5
mariopaolucci <mario.paolucci@gmail.com>
2019-07-25 22:05:50 +0200

    Separating job tests

--- proton-t-tests/src/test/scala/it/cnr/istc/labss/proton/t/tests/TPreachingTests.scala
+++ proton-t-tests/src/test/scala/it/cnr/istc/labss/proton/t/tests/TPreachingTests.scala
@@ -4,7 +4,7 @@ import org.nlogo.headless.HeadlessWorkspace
 import org.nlogo.core.LogoList
 import org.nlogo.api.ScalaConversions.RichSeq
 
-class TJobsTests extends TModelSuite {
+class TPreachingTests extends TModelSuite {
 
  def setup(ws: HeadlessWorkspace): Unit = {
     ws.cmd("""
@@ -91,35 +91,5 @@ class TJobsTests extends TModelSuite {
       """)
       val after = ws.rpt(meanInstDist).asInstanceOf[Number].floatValue
       after - before > 0 shouldBe true
-  }
-
-  test("citizens in workplace are working") { ws =>
-    setup(ws)
-    for (fid <- 1 to 400) {
-      ws.cmd("go")
-      ws.rpt("""
-        count citizens with [ any? out-activity-link-neighbors with [ 
-          [ is-mandatory? ] of my-activity-type and 
-          [ current-activity] of myself = self] 
-        ] -
-        count citizens with  [ [ [is-mandatory? ] of my-activity-type ] of current-activity ]
-      """) shouldBe 0 
-      ws.rpt("""
-        all? citizens with [ any? locations-here with [ shape = "workplace" ] ] [ 
-          [ [ location-type ] of my-activity-type ] of current-activity = "workplace" ]
-        """) shouldBe true  
-      ws.rpt("""
-        max [length [ shape ] of locations-here  ] of activities = 1
-        """) shouldBe true    
-      if (fid % 10 == 0) println(fid)    
-    }
-  }
-
-  test("no homeless") { ws =>
-    setup(ws)
-    ws.cmd("go")
-    ws.rpt("""
-        any? citizens with [ not member? "residence" [ [ location-type ] of my-activity-type ] of activity-link-neighbors  ]
-     """) shouldBe false
   }
 }
