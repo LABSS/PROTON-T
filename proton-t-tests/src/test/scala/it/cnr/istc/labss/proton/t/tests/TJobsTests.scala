fb27958c692a35210e598ddfd512c40651e70ef1
mariopaolucci <mario.paolucci@gmail.com>
2019-07-25 18:14:37 +0200

    Removing mosques and imams from tests

--- proton-t-tests/src/test/scala/it/cnr/istc/labss/proton/t/tests/TJobsTests.scala
+++ proton-t-tests/src/test/scala/it/cnr/istc/labss/proton/t/tests/TJobsTests.scala
@@ -14,11 +14,11 @@ class TJobsTests extends TModelSuite {
     """)
   }
 
-  test("All mosques and community centers staffed") { ws =>
+  test("All propaganda and community centers staffed") { ws =>
     setup(ws)
     ws.rpt("""
       all? activities with [ 
-        [ is-job? and (location-type = "community center"  or location-type = "mosque" or location-type = "radical mosque") ]  of my-activity-type 
+        [ is-job? and (location-type = "community center"  or location-type = "propaganda place") ]  of my-activity-type 
       ] [ 
         any? activity-link-neighbors 
       ] 
@@ -59,33 +59,33 @@ class TJobsTests extends TModelSuite {
     after - before < 0 shouldBe true
   }
 
-  test("Radical imams preach to an effect") { ws =>
+  test("Radicalizers preach to an effect") { ws =>
     setup(ws) 
     for (fid <- 1 to 24 * 3 + 18) {
       ws.cmd(" go ")
       if (fid % 10 == 0) println(fid)    
     }
     ws.cmd("""
-      ask n-of (count citizens / 5 * 
-        count locations with [ shape = "radical mosque" ])
+      ask n-of (count citizens / 5 / 
+        count locations with [ shape = "propaganda place" ])
         citizens with [
-          [ shape ] of locations-here != [ "radical mosque" ] 
+          [ shape ] of locations-here != [ "propaganda place" ] 
         ] [
-          move-to one-of locations with [ shape = "radical mosque" ] 
+          move-to one-of locations with [ shape = "propaganda place" ] 
         ]    
     """)
     val meanInstDist = """
       mean [ value ] of link-set [
         out-topic-link-to ( one-of topics with [ topic-name = "Institutional distrust" ]) 
-      ] of (citizens-on locations with [ shape = "radical mosque" ]) with [
-        [ not (is-job? and location-type = "radical mosque") ] of [ my-activity-type ] of current-activity 
+      ] of (citizens-on locations with [ shape = "propaganda place" ]) with [
+        [ not (is-job? and location-type = "propaganda place") ] of [ my-activity-type ] of current-activity 
       ]
     """
     val before = ws.rpt(meanInstDist).asInstanceOf[Number].floatValue
     ws.cmd("""
       repeat 100 [ 
         ask citizens with [ 
-          [ is-job? and location-type = "radical mosque" ] of [ my-activity-type ] of current-activity 
+          [ is-job? and location-type = "propaganda place" ] of [ my-activity-type ] of current-activity 
         ] [ preach ]
         ]
       """)
