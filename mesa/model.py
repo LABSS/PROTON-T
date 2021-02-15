8a001ffb8bdfca84ad0595f3d3f92b12012b26d3
federico <cecconi.federico@gmail.com>
2021-02-15 13:25:12 +0100

    update load pops data/setup procedure

--- mesa/model.py
+++ mesa/model.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 from typing import Dict, Union, Any
 import time
 from mesa import Model
-from mesa.time import RandomActivation
+from mesa.time import BaseScheduler
 from numpy.random import default_rng
 from entities import Citizen
+from scenario import *
 
 
 class ProtonT(Model):
@@ -16,50 +17,45 @@ class ProtonT(Model):
         self.space_width: int = 40
         self.space_height: int = 40
         self.rng: default_rng = default_rng(self.seed)
-        self.schedule: RandomActivation = RandomActivation(self)
-        # Global definitions
-        # ------------------
-        # Areas: TO DO
+        self.schedule: BaseScheduler = BaseScheduler(self)
+        self.total_citizens: int = number_of_citizens
+
         self.number_of_citizens: int = number_of_citizens
-        self.areas: Dict = {}  # areas hashtable. {key:population}
+        self.local: Dict = {}  # table with values for setup
+        self.population: Dict = {}
+        self.areas = []  # list index of areas, [1,2 ...4]
+        self.area_names: Dict = {}  # names of areas, {1:A, ...}
+        self.area_population: Dict = {}  # population into the areas, {1:9999, ...}
+
+        # todo: add sliders definitions
+        # todo: add all global definitions
+        # self.population_details
+        # self.migrant_muslims_ratio
+        # self.soc_counter
+        # self.soc_online_counter
+        # self.rec_counter
+        # self.printed
+        # self.fail_activity_counter
+        # self.radicalization_threshold
+        # self.police_actions_counter
+        # self.police_action_probability
+        # self.r_t_100
+        # self.r_t_56
 
     def __repr__(self):
         return "ProtonT Model"
 
-    def load_totals(self):
-        print('Load Totals')
-        # load demographic data
-
-    def setup_world(self):
-        print('Load Totals')
-        # load demographic data
-
-    def setup_topics(self):
-        print('Load Totals')
-        # load demographic data
-
-    def load_opinions(self):
-        from scenario import Opinions
-        print('Load Totals')
-        # load demographic data
-
-
-
-    def setup_communities_citiziens(self):
+    def setup_communities_citizens(self):
         for citizen in range(self.number_of_citizens):
             new_citizen = Citizen(self, citizen)
             self.schedule.add(new_citizen)
 
 
-model = ProtonT()
+if __name__ == "__main__":
+    model = ProtonT()
 
-model.load_totals()
-model.setup_world()
-model.setup_topics()
+    load_totals(model)
+    model.setup_communities_citizens()
 
-model.setup_communities_citiziens()
-
-model.load_opinions()
-
-for agent in model.schedule.agents:
-    print(agent)
+    for agent in model.schedule.agents:
+        print(agent)
--- mesa/scenario.py
+++ mesa/scenario.py
@@ -1,4 +1,33 @@
-class Opinions:
+import pandas as pd
 
-    def __init__(self):
-        print(1)
+
+def from_db_to_dict(variable_name, db):
+    table = {}
+    if variable_name == "local":
+        for i in range(len(db)):
+            key = db.iloc[i, 0]
+            if key in table:
+                table[key][db.iloc[i, 1]] = db.iloc[i, 2]
+            else:
+                table[key] = {db.iloc[i, 1]: db.iloc[i, 2]}
+    if variable_name == "population":
+        for i in range(len(db)):
+            table[db.iloc[i, 0]] = [db.iloc[i, 1], db.iloc[i, 2]]
+
+    return table
+
+
+def load_totals(model):
+    data_path = "../inputs/neukolln/data/"
+    local_db = pd.read_csv(data_path+"neighborhoods.csv")
+    model.local = from_db_to_dict("local", local_db)
+    population_db = pd.read_csv(data_path + "neukolln-totals.csv")
+    model.population = from_db_to_dict("population", population_db)
+    model.areas = list(model.population.keys())
+    names = population_db["area_name"].values.tolist()
+    population = population_db["sum(value)"].values.tolist()
+    population_sum = sum(population)
+    for i in range(len(model.areas)):
+        key = model.areas[i]
+        model.area_population[key] = population[i] / population_sum * model.total_citizens
+        model.area_names[key] = names[i]
