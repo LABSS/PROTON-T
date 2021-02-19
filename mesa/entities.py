2eb684fe1e56c02cec0ab1f9d6e730d77675f57e
federico <cecconi.federico@gmail.com>
2021-02-19 09:26:15 +0100

    revised version - from update load pops

--- mesa/entities.py
+++ mesa/entities.py
@@ -1,16 +1,10 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan 27 12:43:13 2021
-
-@author: cecconi.federico
-"""
 import typing
 from mesa import Agent
 if typing.TYPE_CHECKING:
     from model import ProtonT
 
 
-class Locations:
+class Location:
 
     def __init__(self, size=0, shape='null'):
         self.shape: [] = shape
@@ -26,7 +20,7 @@ class Citizen(Agent):
         self.x = 0
         self.y = 0
         self.area: int = 0
-        self.residence: Locations = Locations()
+        self.residence: Location = Location()
 
     def __repr__(self):
         return "Agent: " + str(self.unique_id)
--- mesa/scenario.py
+++ mesa/scenario.py
@@ -1,8 +1,5 @@
-import pandas as pd
-
-
 def from_db_to_dict(variable_name, db):
-    table = {}
+    table = dict()
     if variable_name == "local":
         for i in range(len(db)):
             key = db.iloc[i, 0]
@@ -13,21 +10,6 @@ def from_db_to_dict(variable_name, db):
     if variable_name == "population":
         for i in range(len(db)):
             table[db.iloc[i, 0]] = [db.iloc[i, 1], db.iloc[i, 2]]
-
     return table
 
 
-def load_totals(model):
-    data_path = "../inputs/neukolln/data/"
-    local_db = pd.read_csv(data_path+"neighborhoods.csv")
-    model.local = from_db_to_dict("local", local_db)
-    population_db = pd.read_csv(data_path + "neukolln-totals.csv")
-    model.population = from_db_to_dict("population", population_db)
-    model.areas = list(model.population.keys())
-    names = population_db["area_name"].values.tolist()
-    population = population_db["sum(value)"].values.tolist()
-    population_sum = sum(population)
-    for i in range(len(model.areas)):
-        key = model.areas[i]
-        model.area_population[key] = population[i] / population_sum * model.total_citizens
-        model.area_names[key] = names[i]
