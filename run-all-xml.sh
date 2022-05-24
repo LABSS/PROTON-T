6c8bb141def479583fc747d0abbb786eed5e95f6
Mario <mario.paolucci@gmail.com>
2022-05-24 15:54:33 +0200

    this is the set of actual experiment names launched on labss-simul for the paper

--- run-all-xml.sh
+++ run-all-xml.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 version="rp0.8"
-netlogo="/Applications/NetLogo\ 6.0.4/netlogo-headless.sh"
+netlogo="NetLogo\ 6.1.1/netlogo-headless4_G.sh"
 
 for arg in `ls experiments-xml/*rp0.8.xml`; do
-   eval "$netlogo --model PROTON-T.nlogo --setup-file $arg --table $arg.csv > $arg.out 2>&1 &"	
+   eval "nohup time '"$NLOGO"' --model PROTON-T.nlogo --setup-file $arg --table $arg.`hostname`.`git rev-parse --short HEAD`.csv > $arg.`hostname`.`git rev-parse --short HEAD`.out 2>&1 &"
 done
